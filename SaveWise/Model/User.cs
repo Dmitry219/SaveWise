516f7987498758acd0d3b39ca31d4a57fcf994d7
Dmitry <dima-nazarov-98@list.ru>
2025-04-05 12:03:35 +0300

    Добавлены модели и репозитории пользователя

---
+++ SaveWise/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SaveWise.Model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public int age { get; set; }
+        public DateTime dteCreate { get; set; }
+    }
+}
---
+++ SaveWise/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using Npgsql;
+using SaveWise.Model;
+using System.Data;
+
+namespace SaveWise.Repositories
+{
+    public class UserRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public UserRepository(IConfiguration config)
+        {
+            _connection = new NpgsqlConnection(config.GetConnectionString("Postgres"));
+        }
+
+        public async Task AddUsers(User user)
+        {
+            var sql = @"insert into Users (name, age) 
+                        values (@name, @age)
+                        returning id, dteCreate";
+
+            var result = await _connection.QuerySingleAsync<User>(sql, user);
+            user.id = result.id;
+            user.dteCreate = result.dteCreate;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _connection.QueryAsync<User>("select * from Users");
+        }
+
+        public async Task<User> GetByIdUser(int idUser)
+        {
+            return await _connection.QueryFirstOrDefaultAsync<User>("select * from Users where id = @idUser",
+                new { id = idUser }
+            );
+        }
+
+        public async Task DeleteByIdUser(int idUser)
+        {
+            await _connection.ExecuteAsync("Delete Users where id = @idUser",
+                new { id = idUser }
+            );
+        }
+    }
+}
