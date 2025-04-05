dae1ae20d3e1d284599b87090c0327ccd2170c29
Dmitry <dima-nazarov-98@list.ru>
2025-04-05 12:04:08 +0300

    Добавлены тесты и миграции

---
+++ SaveWise/Migrations/CreateUsersTable.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace SaveWise.Migrations
+{
+    [Migration(202504042333)]
+    public class CreateUsersTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("name").AsString(30).NotNullable()
+                .WithColumn("age").AsInt32().NotNullable()
+                .WithColumn("dteCreate").AsDateTime().WithDefault(SystemMethods.CurrentDateTime);
+        }
+
+        public override void Down()
+        {
+            if (Schema.Table("Users").Exists())
+            {
+                Delete.Table("Users");
+            }
+        }
+    }
+}
